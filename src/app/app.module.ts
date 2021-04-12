ecd012207d98a0d2244672c14087d9faa3295482
Bojan <bsiljanoski98@gmail.com>
2021-04-12 23:45:59 +0200

    Upate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClubmemberComponent } from './clubmember/clubmember.component';
+import { MemberDetailComponent } from './member-detail/member-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClubmemberComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/member.ts
@@ -0,0 +1,14 @@
+export interface Member {
+    id: number;
+    name: string;
+    surname:string;
+    age:number;
+    phone:number;
+    email:string;
+    gender:string;
+    language:string;
+    occupation:string;
+    company:string;
+    username:string;
+    password:number;
+  }
