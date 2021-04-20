0563e6cd654fe4b73263550a549511c873071116
Bojan <bsiljanoski98@gmail.com>
2021-04-20 12:47:36 +0200

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { JokesService } from './chuckapi/jokes.service';
 import { SnackComponent } from './snack/snack.component';
 
 @Component({
@@ -12,42 +11,30 @@ export class AppComponent implements OnInit {
   jokes:any[] = [];
   categories:any[] = [];
 
-  constructor(private jokesService: JokesService, private snackBar: MatSnackBar ) {}
+  constructor( private snackBar: MatSnackBar ) {}
 
 ngOnInit() {
-  this.jokesService.getCategories()
-  .subscribe((categories:any) => {
+  
+  subscribe((categories:any) => {
     this.categories=categories
 
   });
 
-  this.jokesService.getRandomJoke()
-  .subscribe(joke => {
-    this.jokes.push(joke);
-  });
+   
 }
-
   title = 'Club Members';
 
-  
+
   openSnackBar() {
     this.snackBar.openFromComponent(SnackComponent, {
       duration: 500,
     });
   }
-
-  searchByCategory(category: string){
-    this.jokesService.getCategoryJoke(category)
-    .subscribe(joke => {
-      this.jokes=[];
-      this.jokes.push(joke);
-    }
-
       
-      )
   }
+
+
+function subscribe(arg0: (categories: any) => void) {
+  throw new Error('Function not implemented.');
 }
-
-
-
 
